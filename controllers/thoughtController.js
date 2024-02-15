3a4c443e1fc3f4b2ac4772a29594f058163c2771
Traspo <nyaspo8@gmail.com>
2024-02-15 09:31:41 -0500

    delete reactions just deleting 1 now!

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -111,11 +111,13 @@ module.exports = {
   // Remove thought reaction by ID
   async removeReaction(req, res) {
     try {
+      console.log(req.params);
       const thought = await Thought.findOneAndUpdate(
         { _id: req.params.thoughtId },
-        { $pull: { reactions: { reactionId: req.params.reactionID } } },
+        { $pull: { reactions: { reactionID: req.params.reactionId } } },
         { new: true }
       );
+      
 
       if (!thought) {
         return res.status(404).json({ message: 'No thought with this id!' });
